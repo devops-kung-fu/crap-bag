4943d98b44cf4dca6e9a8191ab42c6c2fb34f545
Julio Jimenez <julio@julioj.com>
2021-03-24 22:21:50 -0400

    update

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,12 +4,28 @@ resource "aws_s3_bucket" "no_tags" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kms_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket" "empty_tags" {
   bucket = "empty-tags"
   versioning {
     enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kms_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
   }
   tags = {}
 }
@@ -19,6 +35,14 @@ resource "aws_s3_bucket" "versioning_false" {
   versioning {
     enabled = false
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kms_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
   tags = {
     Name = "versioning-false"
   }
@@ -27,6 +51,14 @@ resource "aws_s3_bucket" "versioning_false" {
 resource "aws_s3_bucket" "versioning_empty" {
   bucket = "versioning-empty"
   versioning {}
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kms_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
   tags = {
     Name = "versioning-empty"
   }
@@ -34,8 +66,25 @@ resource "aws_s3_bucket" "versioning_empty" {
 
 resource "aws_s3_bucket" "versioning_missing" {
   bucket = "versioning-missing"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kms_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
   tags = {
     Name = "versioning-missing"
   }
 }
 
+resource "aws_s3_bucket" "server_side_encryption_missing" {
+  bucket = "server-side-encryption-missing"
+  versioning {
+    enabled = false
+  }
+  tags = {
+    Name = "server-side-encryption-missing"
+  }
+}
